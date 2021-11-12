1729e4bbf18986d12cf330eb4a653050b8577c05
Max Breunig <maxybrun@gmail.com>
2021-11-12 16:25:34 -0500

    added conversion - WIP

---
+++ maxbreunig.github.io-main/weatherapi.js
@@ -0,0 +1,35 @@
+const key = '7b46f096c7a660f265c228386f86e78c';
+if(key=='') document.getElementById('temp').innerHTML = ('Remember to add your api key!');
+
+function weatherBallon( cityID ) {
+	fetch('https://api.openweathermap.org/data/2.5/weather?id=' + cityID+ '&appid=' + key)  
+	.then(function(resp) { return resp.json() }) // Convert data to json
+	.then(function(data) {
+		drawWeather(data);
+	})
+	.catch(function() {
+		// catch any errors
+	});
+}
+function drawWeather( d ) {
+  var celcius = Math.round(parseFloat(d.main.temp)-273.15);
+  var fahrenheit = Math.round(((parseFloat(d.main.temp)-273.15)*1.8)+32);
+  var description = d.weather[0].description; 
+	
+	document.getElementById('description').innerHTML = description;
+	document.getElementById('temp').innerHTML = fahrenheit + '&deg;' + "F";
+	document.getElementById('location').innerHTML = d.name + ", Kentucky";
+  
+  if( description.indexOf('rain') > 0 ) {
+  	document.body.className = 'rainy';
+  } else if( description.indexOf('cloud') > 0 ) {
+  	document.body.className = 'cloudy';
+  } else if( description.indexOf('sunny') > 0 ) {
+  	document.body.className = 'sunny';
+  } else {
+  	document.body.className = 'clear';
+  }
+}
+window.onload = function() {
+	weatherBallon( 4299276 );
+}
